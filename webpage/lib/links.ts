8bfc111292080bf4ebc90c6ae72173a8ca3f779a
johnlivingprooff <johnlivingprooff@gmail.com>
2026-04-09 23:43:35 +0200

    Fix webpage APK release link to published artifact

--- webpage/lib/links.ts
+++ webpage/lib/links.ts
@@ -1,7 +1,7 @@
-export const RELEASE_VERSION = '2026.4.9';
-export const RELEASE_FILENAME = 'app-release.apk';
-export const RELEASE_STATUS = 'ready';
-export const RELEASE_SIZE = 'release APK';
+export const RELEASE_VERSION = '6';
+export const RELEASE_FILENAME = 'pocketflow.apk';
+export const RELEASE_STATUS = 'published';
+export const RELEASE_SIZE = '123 MB APK';
 export const RELEASES_URL = `https://github.com/johnlivingprooff/pocketFlow/releases/download/v${RELEASE_VERSION}/${RELEASE_FILENAME}`;
 const BETA_WHATSAPP_URL = 'https://chat.whatsapp.com/Lv7maaQeZnrBhVDeEIXA0Q';
 
